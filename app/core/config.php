30567c595ac33b5f368ea39f4f9d6a6740e19a78
eliseekn <eliseekn@gmail.com>
2018-11-28 13:08:52 +0000

    Initial commit

--- app/core/config.php
+++ app/core/config.php
@@ -8,14 +8,14 @@ define("ADMIN_PASSWORD", "admin");
 define("ADMIN_EMAIL", "admin@mail.com");
 
 //configuration du blog
-define("WEB_ROOT", "/bloguiste");
+define("WEB_ROOT", "/bloguistecms");
 define("WEB_TITLE", "Mon blog");
 
 //configuration de la base de données
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "bloguiste");
+define("DB_NAME", "bloguistecms");
 		
 //configuration par défault du MVC
 define("DEFAULT_CONTROLLER", "home");
